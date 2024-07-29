66557071dc4a5680599ffa02474a7217aa33e493
boatbomber <zack@boatbomber.com>
2024-07-29 11:38:12 -0700

    Add some movement to the blocks in the demo

--- demo/init.client.lua
+++ demo/init.client.lua
@@ -10,7 +10,7 @@ local CullThrottle = require(Packages:WaitForChild("CullThrottle"))
 local BlocksFolder = Instance.new("Folder")
 BlocksFolder.Name = "Blocks"
 for _ = 1, 100 do
-	local groupOrigin = Vector3.new(math.random(-800, 800), math.random(50, 500), math.random(-800, 800))
+	local groupOrigin = Vector3.new(math.random(-800, 800), math.random(60, 400), math.random(-800, 800))
 	for _ = 1, 100 do
 		local part = Instance.new("Part")
 		part.Size = Vector3.new(5, 5, 5)
@@ -48,11 +48,27 @@ end)
 
 -- Each frame, we'll ask CullThrottle for all the objects that should be updated this frame,
 -- and then rotate them accordingly with BulkMoveTo.
-RunService.RenderStepped:Connect(function()
+local ROT_SPEED = math.rad(90)
+local MOVE_AMOUNT = 10
+RunService.RenderStepped:Connect(function(frameDeltaTime)
 	local blocks, cframes = {}, {}
-	for block, deltaTime in FloatingBlocksUpdater:getObjectsToUpdate() do
+	local now = os.clock() / 2
+	local movement = math.sin(now) * MOVE_AMOUNT
+	for block, objectDeltaTime in FloatingBlocksUpdater:getObjectsToUpdate() do
+		if objectDeltaTime > 0.4 then
+			-- This object hasn't been updated in a while, so if we were to animate
+			-- it based on the objectDT, it would jump to where it "should" be now.
+			-- For our purposes, we'd rather it just pick up from where it is and avoid popping
+			objectDeltaTime = frameDeltaTime
+		end
+
 		table.insert(blocks, block)
-		table.insert(cframes, block.CFrame * CFrame.Angles(0, math.rad(180) * deltaTime, 0))
+		table.insert(
+			cframes,
+			block.CFrame
+				* CFrame.new(0, movement * objectDeltaTime, 0)
+				* CFrame.Angles(0, ROT_SPEED * objectDeltaTime, 0)
+		)
 	end
 
 	workspace:BulkMoveTo(blocks, cframes, Enum.BulkMoveMode.FireCFrameChanged)
